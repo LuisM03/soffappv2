a75b1d3f5f102292b849984dac093b0b45106a9f
jaider30 <jaidercg30@gmail.com>
2023-06-25 20:47:07 -0500

    insumos crud complet

---
+++ soffapp/Controllers/ReportesInsumoController.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using soffapp.Models;
+
+public class ReportesInsumoController : Controller
+{
+    private readonly SoffDataContext _context;
+
+    public ReportesInsumoController(SoffDataContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+
+
+    {
+
+        //if (!User.Identity.IsAuthenticated)
+        //{
+        //    return Redirect("/");
+        //}
+        //else
+        //{
+
+            var insumos = _context.Insumos.ToList();
+            //var productos = _context.Productos.ToList();
+            //var ventas = _context.Ventas.ToList();
+
+            ViewData["Insumos"] = insumos;
+            //ViewData["Productos"] = productos;
+            //ViewData["Ventas"] = ventas;
+
+            return View();
+        //}
+    }
+}
+
--- soffapp/Models/Insumo.cs
+++ soffapp/Models/Insumo.cs
@@ -6,21 +6,28 @@ namespace soffapp.Models;
 
 public partial class Insumo
 {
-    
+
     public long IdInsumo { get; set; }
-    
+
+
     public long IdProveedor { get; set; }
-    
+
+    [Required]
     public string Nombre { get; set; } = null!;
-    
+
+    [DataType(DataType.Date)]
     public DateTime FechaCaducidad { get; set; }
-    
+
+    [Required]
     public int Stock { get; set; }
-    
+
+    [Required]
     public string Medida { get; set; } = null!;
-    
+
+    [Required]
     public decimal Precio { get; set; }
     
+
     public bool? Estado { get; set; }
 
     public virtual ICollection<DetalleInsumo> DetalleInsumos { get; set; } = new List<DetalleInsumo>();
