a27af49423e367987f1d1b3aba0a890d216b7558
Luis Montiel <83305447+LuisM03@users.noreply.github.com>
2023-06-25 21:00:00 -0500

    Ultimos cambios de venta

--- soffapp/Controllers/VentasController.cs
+++ soffapp/Controllers/VentasController.cs
@@ -18,6 +18,8 @@ namespace soffapp.Controllers
 
         public IActionResult Index()
         {
+
+
             var ventas = context.Venta
                 .Join(
                   context.OrdenVenta,
@@ -63,20 +65,20 @@ namespace soffapp.Controllers
             }
         }
 
-        public IActionResult ReportePDF()
-        {
-            PdfDocument document = new PdfDocument();
-            PdfPage page = document.AddPage();
-            XGraphics gfx = XGraphics.FromPdfPage(page);
-            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+        //public IActionResult ReportePDF()
+        //{
+        //    PdfDocument document = new PdfDocument();
+        //    PdfPage page = document.AddPage();
+        //    XGraphics gfx = XGraphics.FromPdfPage(page);
+        //    XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
 
-            gfx.DrawString("Hello, World!", font, XBrushes.Black,
-                new XRect(0, 0, page.Width, page.Height),
-                XStringFormat.Center);
-            string filename = "HelloWorld.pdf";
-            document.Save(filename);
-            return View(document);
-        }
+        //    gfx.DrawString("Hello, World!", font, XBrushes.Black,
+        //        new XRect(0, 0, page.Width, page.Height),
+        //        XStringFormat.Center);
+        //    string filename = "HelloWorld.pdf";
+        //    document.Save(filename);
+        //    return View(document);
+        //}
 
         public async Task<IActionResult> Delete(string id) 
         {
