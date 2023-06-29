c021e912d791926398109afd6ec4f02ffa16dc3b
evelynmontoyav <evemontoya10@ielasnieves.edu.co>
2023-06-28 21:28:39 -0500

    ultimos detalles de compras

--- soffapp/Controllers/OrdenComprasController.cs
+++ soffapp/Controllers/OrdenComprasController.cs
@@ -20,86 +20,108 @@ namespace soffapp.Controllers
 
         public async Task<IActionResult> Create(int id)
         {
-            string? _urlData = HttpContext.Request.Path.Value;
-            string[] splitData = _urlData.Split('/');
-            var IdCompra = int.Parse(splitData[splitData.Length - 1]);
-            ViewBag.IdCompra = IdCompra;
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                string? _urlData = HttpContext.Request.Path.Value;
+                string[] splitData = _urlData.Split('/');
+                var IdCompra = int.Parse(splitData[splitData.Length - 1]);
+                ViewBag.IdCompra = IdCompra;
 
-            var OrdenesCompras = context.OrdenCompras
-                .Where(o => o.IdCompra == IdCompra)
-                .Join(
-                    context.Insumos,
-                    orden => orden.IdInsumo,
-                    insumo => insumo.IdInsumo,
-                    (orden, insumo) => new
-                    {
-                        insumo.Nombre,
-                        orden.Cantidad,
-                        orden.PrecioUnitario,
-                        orden.Total,
-                        orden.IdOrden,
-                        orden.IdCompra
-                    })
-                .ToList();
+                var OrdenesCompras = context.OrdenCompras
+                    .Where(o => o.IdCompra == IdCompra)
+                    .Join(
+                        context.Insumos,
+                        orden => orden.IdInsumo,
+                        insumo => insumo.IdInsumo,
+                        (orden, insumo) => new
+                        {
+                            insumo.Nombre,
+                            orden.Cantidad,
+                            orden.PrecioUnitario,
+                            orden.Total,
+                            orden.IdOrden,
+                            orden.IdCompra
+                        })
+                    .ToList();
 
-            ViewBag.Detalles = OrdenesCompras;
-            ViewBag.Insumos = await context.Insumos.Select(x => new { x.IdInsumo, x.Nombre }).ToListAsync();
-            ViewBag.Proveedores = await context.Proveedors.Select(x => new { x.IdProveedor, x.Nombre }).ToListAsync();
-            Tuple<OrdenCompra, Compra> models = new Tuple<OrdenCompra, Compra>(new OrdenCompra(), new Compra());
-            return View(models);
+                ViewBag.Detalles = OrdenesCompras;
+                ViewBag.Insumos = await context.Insumos.Select(x => new { x.IdInsumo, x.Nombre }).ToListAsync();
+                ViewBag.Proveedores = await context.Proveedors.Select(x => new { x.IdProveedor, x.Nombre }).ToListAsync();
+                Tuple<OrdenCompra, Compra> models = new Tuple<OrdenCompra, Compra>(new OrdenCompra(), new Compra());
+                return View(models);
+            }
         }
 
 
         public async Task<IActionResult> Details()
         {
-            string? _urlData = HttpContext.Request.Path.Value;
-            string[] splitData = _urlData.Split('/');
-            var IdCompra = int.Parse(splitData[splitData.Length - 1]);
-
-            var Ordenes = context.OrdenCompras
-                .Where(o => o.IdCompra == IdCompra)
-                .Join(
-                    context.Insumos,
-                    orden => orden.IdInsumo,
-                    insumo => insumo.IdInsumo,
-                    (orden, insumo) => new
-                    {
-                        insumo.Nombre,
-                        orden.Cantidad,
-                        orden.PrecioUnitario,
-                        orden.Total,
-                        orden.IdOrden,
-                        orden.IdCompra
-                    }).ToList();
-            var compra = context.Compras.Where(o => o.IdCompra == IdCompra).Select(x => new { x.IdCompra, x.IdProveedor, x.FechaCompra, x.Total }).ToList();
-            ViewBag.Ordenes = Ordenes;
-            ViewBag.IdCompra = IdCompra;
-            foreach (var item in compra)
+            if (!User.Identity.IsAuthenticated)
             {
-                ViewBag.TotalCompra = item.Total;
+                return Redirect("/");
             }
+            else
+            {
+                string? _urlData = HttpContext.Request.Path.Value;
+                string[] splitData = _urlData.Split('/');
+                var IdCompra = int.Parse(splitData[splitData.Length - 1]);
 
-            return View();
+                var Ordenes = context.OrdenCompras
+                    .Where(o => o.IdCompra == IdCompra)
+                    .Join(
+                        context.Insumos,
+                        orden => orden.IdInsumo,
+                        insumo => insumo.IdInsumo,
+                        (orden, insumo) => new
+                        {
+                            insumo.Nombre,
+                            orden.Cantidad,
+                            orden.PrecioUnitario,
+                            orden.Total,
+                            orden.IdOrden,
+                            orden.IdCompra
+                        }).ToList();
+                var compra = context.Compras.Where(o => o.IdCompra == IdCompra).Select(x => new { x.IdCompra, x.IdProveedor, x.FechaCompra, x.Total }).ToList();
+                ViewBag.Ordenes = Ordenes;
+                ViewBag.IdCompra = IdCompra;
+                foreach (var item in compra)
+                {
+                    ViewBag.TotalCompra = item.Total;
+                }
+
+                return View();
+            }
         }
 
         [HttpPost]
         public IActionResult Create([Bind(Prefix = "Item1")] OrdenCompra ordenCompra, [Bind(Prefix = "Item2")] Compra compra, Insumo insumo)
         {
-            var insummo = context.Insumos.Where(x => x.IdInsumo == ordenCompra.IdInsumo).FirstOrDefault()!;
-            ordenCompra.PrecioUnitario = insummo.Precio;
-            ordenCompra.Total = insummo.Precio * Int32.Parse(ordenCompra.Cantidad);
-            context.Add(ordenCompra);
-            context.SaveChanges();
-
-            var insumoo = context.Insumos.FirstOrDefault(i => i.IdInsumo == ordenCompra.IdInsumo);
-            if (insumoo != null)
+            if (ordenCompra.IdInsumo != null && ordenCompra.Cantidad != null)
             {
-                insumoo.Stock += int.Parse(ordenCompra.Cantidad);
-                context.Update(insumoo);
+                var insummo = context.Insumos.Where(x => x.IdInsumo == ordenCompra.IdInsumo).FirstOrDefault()!;
+                ordenCompra.PrecioUnitario = insummo.Precio;
+                ordenCompra.Total = insummo.Precio * Int32.Parse(ordenCompra.Cantidad);
+                context.Add(ordenCompra);
                 context.SaveChanges();
-            }
 
-            return Redirect($"/OrdenCompras/Create/{ordenCompra.IdCompra}");
+                var insumoo = context.Insumos.FirstOrDefault(i => i.IdInsumo == ordenCompra.IdInsumo);
+                if (insumoo != null)
+                {
+                    insumoo.Stock += int.Parse(ordenCompra.Cantidad);
+                    context.Update(insumoo);
+                    context.SaveChanges();
+                }
+
+                return Redirect($"/OrdenCompras/Create/{ordenCompra.IdCompra}");
+            }else
+            {
+                ViewData["MessageInsumo"] = "Debe ingresar un insumo";
+                ViewData["MessageCantidad"] = "Debe ingrsar la cantidad";
+                return Redirect($"/OrdenCompras/Create/{ordenCompra.IdCompra}");
+            }
         }
         public async Task<IActionResult> Delete(String id, int otroId)
         {
@@ -118,9 +140,13 @@ namespace soffapp.Controllers
 
         public IActionResult ConfirmSale([Bind(Prefix = "Item2")] Compra compra)
         {
+            var Compra = context.Compras.Where(x => x.IdCompra == compra.IdCompra).FirstOrDefault()!;
             if (!ModelState.IsValid)
             {
-                var Compra = context.Compras.Where(x => x.IdCompra == compra.IdCompra).FirstOrDefault()!;
+                if(compra.Total == 0)
+                {
+                    return Redirect($"/OrdenCompras/Create/{compra.IdCompra}");
+                }
                 Compra.IdProveedor = compra.IdProveedor;
                 Compra.Total = compra.Total;
                 context.Update(Compra);
@@ -129,7 +155,7 @@ namespace soffapp.Controllers
             }
             else
             {
-                return Redirect("/Compras");
+                return View();
             }
         }
     }
--- soffapp/Models/OrdenCompra.cs
+++ soffapp/Models/OrdenCompra.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace soffapp.Models;
 
 public partial class OrdenCompra
 {
     public long IdOrden { get; set; }
-
+    [Required(ErrorMessage = "Este campo es obligatorio")]
     public long IdCompra { get; set; }
-
+    [Required(ErrorMessage = "Este campo es obligatorio")]
     public long IdInsumo { get; set; }
+    [Remote(action: "Create", controller: "OrdenVenta")]
+    [Required(ErrorMessage = "Debe ingresar una cantidad de la orden")]
+    [Range(1, int.MaxValue, ErrorMessage = "Debe ser mayor a uno")]
 
     public string Cantidad { get; set; } = null!;
 
     public decimal PrecioUnitario { get; set; }
 
-    public decimal? Total { get; set; }
+    public decimal Total { get; set; }
 
     public virtual Compra IdCompraNavigation { get; set; } = null!;
 
